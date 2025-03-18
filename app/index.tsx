c54e329786388450677cc3861759c2e6026c671e
Aryan M. <aryanmanek4@gmail.com>
2025-03-18 10:09:34 +0530

    fix

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ import { StatusBar } from "expo-status-bar";
 
 const App = () => {
   return (  
-    <View className="flex-1 justify-center items-center bg-blue-400">
+    <View className="flex-2 justify-center items-center bg-blue-400">
       <Text className="text-3xl font-bold">RootLayout</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{color: 'blue'}}>Go to Profile Page</Link>
